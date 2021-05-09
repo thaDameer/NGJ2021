0e8a402d067d4353fc1bdd726ce9baa08c30b2fe
thaDameer <38245328+thaDameer@users.noreply.github.com>
2021-05-09 10:08:49 +0200

    seagull animation

---
+++ Assets/Scripts/Seagull.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Seagull : MonoBehaviour
+{
+    [SerializeField] private Rigidbody myRigidbody;
+
+    public void GetClosestTurtleOrEgg()
+    {
+        
+    }
+}
